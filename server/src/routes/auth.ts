cdfa7d186f6da3bd2f2159d0a76ba9b358f98c0c
kofta999 <mostafaxxx555@gmail.com>
2024-08-17 17:53:47 +0300

    Fix CORS and env vars loading

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 
 // Register
 router.post('/register', async (req, res) => {
+    console.log('here')
   const { email, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,35 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import authRoutes from './routes/auth';
-import deckRoutes from './routes/decks';
-import cardRoutes from './routes/cards';
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/auth";
+import deckRoutes from "./routes/decks";
+import cardRoutes from "./routes/cards";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 4000;
 
+dotenv.config();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.json());
 
-app.use('/auth', authRoutes);
-app.use('/decks', deckRoutes);
-app.use('/cards', cardRoutes);
+app.use("/auth", authRoutes);
+app.use("/decks", deckRoutes);
+app.use("/cards", cardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
