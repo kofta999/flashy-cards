53de5fd20b1c13c1c729c7b6f3b77c388303ecd9
kofta999 <mostafaxxx555@gmail.com>
2024-08-17 18:33:41 +0300

    Add route guards for auth routes

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,11 +3,13 @@ import {
   createRoute,
   createRouter,
   Outlet,
+  redirect,
   RouterProvider,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { isAuth } from "./services/authService";
 
 const rootRoute = createRootRoute({
   component: () => (
@@ -22,15 +24,35 @@ const registerRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/register",
   component: Register,
+  beforeLoad: () => {
+    if (isAuth()) {
+      throw redirect({ to: "/home" });
+    }
+  },
 });
 
 const loginRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/login",
   component: Login,
+  beforeLoad: () => {
+    if (isAuth()) {
+      throw redirect({ to: "/home" });
+    }
+  },
 });
 
-const routeTree = rootRoute.addChildren([registerRoute, loginRoute]);
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/home",
+  beforeLoad: () => {
+    if (!isAuth()) {
+      throw redirect({ to: "/login" });
+    }
+  },
+});
+
+const routeTree = rootRoute.addChildren([registerRoute, loginRoute, homeRoute]);
 
 const router = createRouter({ routeTree });
 
