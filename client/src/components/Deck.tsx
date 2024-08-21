4995e7d83135923ffb0dbdffdf586da5dbf584b6
kofta999 <mostafaxxx555@gmail.com>
2024-08-21 19:01:18 +0300

    Add fetch logic for deck component

--- client/src/components/Deck.tsx
+++ client/src/components/Deck.tsx
@@ -1,38 +1,63 @@
-import { IDeck } from "@/types";
 import { useParams } from "@tanstack/react-router";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getDeck } from "@/services/decksService";
+import { useToast } from "./ui/use-toast";
+import { Loader2 } from "lucide-react";
 
 export default function Deck() {
   const { deckId } = useParams({ from: "/home/decks/$deckId" });
 
-  // TODO: Fetch
+  const {
+    data: deck,
+    error,
+    status,
+  } = useQuery({
+    queryKey: ["deck"],
+    queryFn: () => getDeck(parseInt(deckId)),
+    initialData: { id: 0, title: "", cards: [], description: "" },
+  });
+  const { toast } = useToast();
 
-  const deck: IDeck = {
-    id: parseInt(deckId),
-    title: "Basic Japanese",
-    description: "This deck contains basic Japanese vocabulary",
-    cards: [
-      { id: "1", front: "こんにちは", back: "Hello", completed: false },
-      { id: "2", front: "ありがとう", back: "Thank you", completed: false },
-      { id: "3", front: "さようなら", back: "Goodbye", completed: false },
-      { id: "4", front: "はい", back: "Yes", completed: false },
-      { id: "5", front: "いいえ", back: "No", completed: false },
-    ],
-  };
+  // const deck: IDeck = {
+  //   id: parseInt(deckId),
+  //   title: "Basic Japanese",
+  //   description: "This deck contains basic Japanese vocabulary",
+  //   cards: [
+  //     { id: "1", front: "こんにちは", back: "Hello", completed: false },
+  //     { id: "2", front: "ありがとう", back: "Thank you", completed: false },
+  //     { id: "3", front: "さようなら", back: "Goodbye", completed: false },
+  //     { id: "4", front: "はい", back: "Yes", completed: false },
+  //     { id: "5", front: "いいえ", back: "No", completed: false },
+  //   ],
+  // };
+
+  useEffect(() => {
+    if (status === "error" && error) {
+      console.error(error);
+      toast({
+        title: "Error happened while fetching deck",
+        description: "Please try again",
+        variant: "destructive",
+      });
+    }
+
+    if (status === "success") {
+      toast({ title: "Loaded successfully" });
+    }
+  }, [status, error, toast]);
 
   // States
-  const [status, setStatus] = useState<"idle" | "reviewing" | "done">("idle");
-  const [currentCardIndex, setCurrentCardIndex] = useState(
-    parseInt(deck.cards[0].id),
-  );
+  const [state, setState] = useState<"idle" | "reviewing" | "done">("idle");
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [showBack, setShowBack] = useState(false);
 
   const handleDoneCard = () => {
     setShowBack(false);
 
     if (currentCardIndex === deck.cards.length - 1) {
-      setStatus("done");
+      setState("done");
     } else {
       setCurrentCardIndex((curr) => curr + 1);
     }
@@ -45,7 +70,7 @@ export default function Deck() {
 
         <p>{deck.description}</p>
 
-        <Button onClick={() => setStatus("reviewing")}>Start</Button>
+        <Button onClick={() => setState("reviewing")}>Start</Button>
       </>
     );
   }
@@ -55,7 +80,7 @@ export default function Deck() {
     return (
       <>
         <h1 className="text-3xl">{card.front}</h1>
-        {showBack && <p text-xl>{card.back}</p>}
+        {showBack && <p className="text-xl">{card.back}</p>}
 
         <div className="flex justify-center items-center gap-10">
           {showBack ? (
@@ -90,13 +115,18 @@ export default function Deck() {
 
   return (
     <div className="border rounded p-5 md:w-1/2 lg:w-1/3 md:mx-auto mx-5 flex flex-col justify-between text-center min-h-[50dvh]">
-      {status === "idle" ? (
-        <Idle />
-      ) : status === "reviewing" ? (
-        <Card />
-      ) : (
-        <Done />
+      {deck.id === 0 && (
+        <Loader2 className=" mx-auto animate-spin w-10 h-10 mt-10 mb-10" />
       )}
+
+      {deck.id !== 0 &&
+        (state === "idle" ? (
+          <Idle />
+        ) : state === "reviewing" ? (
+          <Card />
+        ) : (
+          <Done />
+        ))}
     </div>
   );
 }
--- client/src/services/decksService.ts
+++ client/src/services/decksService.ts
@@ -1,5 +1,5 @@
 import { API_URL } from "@/consts";
-import { IDeckItem } from "@/types";
+import { IDeck, IDeckItem } from "@/types";
 import { authedClient } from "./authService";
 
 const DECKS_API_URL = API_URL + "/decks";
@@ -7,6 +7,19 @@ const DECKS_API_URL = API_URL + "/decks";
 export async function getDecks() {
   try {
     const { data } = await authedClient.get<IDeckItem[]>(DECKS_API_URL);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getDeck(deckId: number) {
+  try {
+    const { data } = await authedClient.get<IDeck>(
+      DECKS_API_URL + "/" + deckId,
+    );
+
     return data;
   } catch (error) {
     console.error(error);
