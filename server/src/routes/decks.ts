d3a709cfacc2ddfba7c242ff07215fa2c17dc7fb
kofta999 <mostafaxxx555@gmail.com>
2024-08-21 16:07:02 +0300

    Update schema and add get decks route

--- server/src/routes/decks.ts
+++ server/src/routes/decks.ts
@@ -1,12 +1,40 @@
-import express from 'express';
-import prisma from '../models/prismaClient';
-import authenticateJWT from '../middleware/authMiddleware';
+import express from "express";
+import prisma from "../models/prismaClient";
+import authenticateJWT from "../middleware/authMiddleware";
 
 const router = express.Router();
 
+// Get decks
+router.get("/", authenticateJWT, async (req, res) => {
+  const ownerId = (req as any).user.id;
+
+  try {
+    const decks = await prisma.deck.findMany({
+      where: { ownerId },
+      include: {
+        _count: { select: { cards: { where: { completed: false } } } },
+      },
+    });
+
+    const formattedDecks = decks.map((deck) => ({
+      id: deck.id,
+      title: deck.title,
+      dueCardsCount: deck._count.cards,
+    }));
+
+    res.status(200).json(formattedDecks);
+  } catch (error) {
+    console.error("Error creating deck:", error);
+    res.status(500).json({
+      error: "Failed to fetch decks",
+      details: (error as Error).message,
+    });
+  }
+});
+
 // Create Deck
-router.post('/', authenticateJWT, async (req, res) => {
-  const { name } = req.body;
+router.post("/", authenticateJWT, async (req, res) => {
+  const { name, description } = req.body;
   const userId = req.user?.id;
 
   if (!userId) return res.sendStatus(401); // Unauthorized
@@ -15,18 +43,22 @@ router.post('/', authenticateJWT, async (req, res) => {
     const deck = await prisma.deck.create({
       data: {
         title: name,
+        description,
         ownerId: userId,
       },
     });
     res.status(201).json(deck);
   } catch (error) {
     console.error("Error creating deck:", error);
-    res.status(500).json({ error: 'Failed to create deck', details: (error as Error).message });
+    res.status(500).json({
+      error: "Failed to create deck",
+      details: (error as Error).message,
+    });
   }
 });
 
 // Edit Deck
-router.put('/:id', authenticateJWT, async (req, res) => {
+router.put("/:id", authenticateJWT, async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
@@ -38,20 +70,26 @@ router.put('/:id', authenticateJWT, async (req, res) => {
     res.json(deck);
   } catch (error) {
     console.error("Error updating deck:", error);
-    res.status(500).json({ error: 'Failed to update deck', details: (error as Error).message });
+    res.status(500).json({
+      error: "Failed to update deck",
+      details: (error as Error).message,
+    });
   }
 });
 
 // Delete Deck
-router.delete('/:id', authenticateJWT, async (req, res) => {
+router.delete("/:id", authenticateJWT, async (req, res) => {
   const { id } = req.params;
 
   try {
     await prisma.deck.delete({ where: { id: Number(id) } });
-    res.status(204).send(); 
+    res.status(204).send();
   } catch (error) {
     console.error("Error deleting deck:", error);
-    res.status(500).json({ error: 'Failed to delete deck', details: (error as Error).message });
+    res.status(500).json({
+      error: "Failed to delete deck",
+      details: (error as Error).message,
+    });
   }
 });
 
